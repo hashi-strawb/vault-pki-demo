a54c516d94b9b210d1b975c06a0447cfd3f442e9
Lucy Davinhart <lucy@lucymhdavies.com>
2021-05-16 20:38:54 +0100

    Fix approle

--- terraform/approles.tf
+++ terraform/approles.tf
@@ -7,9 +7,22 @@ resource "vault_approle_auth_backend_role" "pki-issue-all" {
   role_name      = "pki-issue-all"
   token_policies = ["pki-issue-all"]
   token_ttl      = 300
-
-  # Not something you would usually do in Production
-  # but simplifies things for the demonstration
-  bind_secret_id = false
   role_id        = "pki-issue-all"
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "pki-issue-all" {
+  backend   = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.approle.pki-issue-all
+
+  # We would not normally want to hard-code a Secret ID here
+  # But because we reference it in other parts of the demonstration, it makes
+  # things easier
+  secret_id = "c9b3d349-c0cd-4763-863b-7babaeda82ad"
+
+  metadata = <<EOT
+{
+  "terraform": "true",
+  "resource": "vault_approle_auth_backend_role_secret_id.pki-issue-all"
+}
+EOT
 }
