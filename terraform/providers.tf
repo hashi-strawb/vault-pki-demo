cf2fa97c2cd1f21c2668f485b46c8720eeec62f3
Lucy Davinhart <lucy@lucymhdavies.com>
2021-05-16 19:52:07 +0100

    Output VAULT_ADDR as a test

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,9 +4,30 @@ terraform {
       source  = "hashicorp/vault"
       version = "2.19.1"
     }
+    environment = {
+      source  = "EppO/environment"
+      version = "1.1.0"
+    }
   }
 }
 
-provider "vault" {
-  # Configuration options
+provider "vault" {}
+
+
+# Get the Vault Address, so we can use it in other resources
+#
+# This relies on the address being set via the VAULT_ADDR environment variable,
+# so it's slightly brittle, but I'm not aware of any way of getting the address
+# out of the Provider itself
+
+data "environment_variables" "address" {
+  filter = "VAULT_ADDR"
+}
+
+locals {
+  vault_addr = data.environment_variables.address.items["VAULT_ADDR"]
+}
+
+output "vault_addr" {
+  value = local.vault_addr
 }
