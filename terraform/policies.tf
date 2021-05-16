1ba7de2e38e0ef3982356c45ba3695f8afe9db13
Lucy Davinhart <lucy@lucymhdavies.com>
2021-05-16 20:30:20 +0100

    Update pki-issue-all

--- terraform/policies.tf
+++ terraform/policies.tf
@@ -2,14 +2,22 @@ resource "vault_policy" "pki-issue-all" {
   name = "pki-issue-all"
 
   policy = <<EOT
+# List enabled secret engines
+path "sys/mounts" {
+  capabilities = [ "read", "list" ]
+}
+
+# Issue certificates from any CA
 path "pki/inter/issue/+" {
   capabilities = ["update"]
 }
 
+# Read details for any PKI Role
 path "pki/inter/roles/+" {
   capabilities = ["read"]
 }
 
+# List all PKI Roles
 path "pki/inter/roles" {
   capabilities = ["list"]
 }
