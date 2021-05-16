824e5a45f4639164d4b5c32fe5d29705e0bc6c8e
Lucy Davinhart <lucy@lucymhdavies.com>
2021-05-16 20:35:41 +0100

    pki_all user

--- terraform/users.tf
+++ terraform/users.tf
@@ -1,3 +1,14 @@
 resource "vault_auth_backend" "userpass" {
   type = "userpass"
 }
+
+resource "vault_generic_endpoint" "user_pki_all" {
+  path = "auth/userpass/users/pki_all"
+
+  data_json = <<EOT
+{
+  "password": "superSecretPassword",
+  "policies": "admin,pki-issue-all",
+}
+EOT
+}
